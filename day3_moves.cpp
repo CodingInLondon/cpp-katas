0205468551c102ed5838cd895c32e01d962c2e3f
CodingInLondon <github@golios.com>
2026-04-10 16:24:06 +0100

    Added first files to repo

---
+++ day3_moves.cpp
@@ -0,0 +1,20 @@
+#include <optional>
+#include <string>
+
+// std::optional<int> parse(const std::string& s){
+//     return 2;
+// }
+
+int parse(const std::string& s){
+    return 2;
+}
+
+
+int main() {
+    auto v = parse("123");
+    // ...
+
+    return 1;
+}
+
+
